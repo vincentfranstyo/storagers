6f18b4d629bb0a3491d9babd07851277e30874b6
Vincent Franstyo <sorakiriko28814@gmail.com>
2023-07-25 18:31:43 +0700

    feat(history): completed History

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -6,14 +6,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\History;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class HistoryController extends Controller
 {
-    public function index(User $user)
+    public function index()
     {
         // filter by user_id
+        $user = Auth::user();
         $histories = History::all()->filter(function ($history) use ($user) {
-            return $history->user_id == $user->id;
+            return $history->user_id == $user['id'];
         });
 
         return view('components/history', ['histories' => $histories]);
